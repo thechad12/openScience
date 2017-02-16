1d6bd6a02469c210e2b48ba72bd174841da9025f
Kevin Pasquarella <pasquarella4292@gmail.com>
2017-02-15 20:03:55 -0500

    initial commit

---
+++ models.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import sqlalchemy
+from sqlalchemy import Column, ForeignKey, Integer
+from sqlalchemy.orm import relationship
+import sys
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/cosmos'
+#db = sqlalchemy(app)
+
+class User(db.Model):
+	__tablename__ = 'user'
+	id = db.Column(db.Integer, primary_key=True)
+	email = db.Column(db.String(120), unique=True)
+	first_name = db.Column(db.String(120))
+	last_name = db.Column(db.String(120))
+
+	def __init__(self,email):
+		self.email = email
+
+	def __repr__(self):
+		return '<E-mail %r>' % self.email
+
+class Post(db.Model):
+	__tablename__ = 'post'
+	id = db.Column(db.Integer, primary_key=True)
+	text = db.Column(db.String(2000))
+	title = db.Column(db.String(250))
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
+
+	@property
+	def serialize(self):
+		return {
+			'title': self.title,
+			'id': self.id,
+			'user_id': self.user_id,
+			'text': self.text
+		}
