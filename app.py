675fc023116a70f4e936287ff615b5872c236fc6
Kevin Pasquarella <pasquarella4292@gmail.com>
2017-08-07 20:16:12 -0400

    formatting fixes

--- app.py
+++ app.py
@@ -34,13 +34,8 @@ app.config['OAUTH_CREDENTIALS'] = {
     }
 }
 
-###
-# Routing for your application.
-###
-
 @app.route('/')
 def home():
-    """Render website's home page."""
     return render_template('home.html')
 
 @app.route('/user/<email>')
@@ -55,7 +50,6 @@ def user(email):
 
 @app.route('/about/')
 def about():
-    """Render the website's about page."""
     return render_template('about.html')
 
 @app.route('/authorize/<provider>')
@@ -120,17 +114,12 @@ def follow(email):
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
-    """Send your static text file."""
     file_dot_text = file_name + '.txt'
     return app.send_static_file(file_dot_text)
 
 
 @app.after_request
 def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=600'
     return response
@@ -138,7 +127,6 @@ def add_header(response):
 
 @app.errorhandler(404)
 def page_not_found(error):
-    """Custom 404 page."""
     return render_template('404.html'), 404
 
 
